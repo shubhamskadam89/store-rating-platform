78b722e973fe216a217055f4571145ce22e2763b
Shubham Kadam <shubhamskadam89@gmail.com>
2026-09-02 10:09:07 +0530

    feat: implement store creation endpoint

--- backend/src/stores/stores.controller.ts
+++ backend/src/stores/stores.controller.ts
@@ -1,1 +1,19 @@
-export class StoresController {}
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import { RolesGuard } from '../auth/roles.guard';
+import { StoresService } from './stores.service';
+import { Role } from '@prisma/client';
+import { CreateStoreDto } from './dto/create-store.dto';
+import { Roles } from '../auth/roles.decorator';
+
+@Controller('stores')
+@UseGuards(JwtAuthGuard, RolesGuard)
+export class StoresController {
+  constructor(private readonly storesService: StoresService) {}
+
+  @Post()
+  @Roles(Role.SYSTEM_ADMIN)
+  create(@Body() createStoreDto: CreateStoreDto) {
+    return this.storesService.create(createStoreDto);
+  }
+}
